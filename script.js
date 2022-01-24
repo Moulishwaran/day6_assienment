679f20b465a368b6090590d070a65b4a1bbd3ebd
Moulishwaran <88322031+Moulishwaran@users.noreply.github.com>
2022-01-24 22:10:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+//1//Find the culprit
+
+//alert( “I’m JavaScript!’);
+
+
+
+//2//Find the culprit and invoke the alert
+
+ //alert(“I’m invoked!”);
+
+
+//3 Explain the below how it works
+
+ //alert("I'm JavaScript!");
+ //alert('Hello');
+ //alert(`World`);
+ //alert(3+1+2);
+
+
+
+//4 Fix the below to alert Guvi geek
+
+
+//let  fname = "Guvi", lname = "geek"
+//admin = fname+lname;
+//alert( admin );
+
+//5 Fix the below to alert hello Guvi geek
+//let  fname = "Guvi", lname = "geek";
+
+//admin = fname+lname;
+//alert( 'hello ${ admin }' );
+
+// 6 Fix the below to alert sum of two numbers
+
+//let a = prompt("First number");
+//let b = prompt("Second number");
+//alert(a + b);
+
+// 7 Change the code to print all the gifts
+
+//let gifts = ["teddy bear", "drone", "doll"];
+//for (let i = 0; i < 3; i++) {
+  //console.log(gifts);
+//}
+
+// 8  Fix the code to disarm the bomb.
+
+let countdown = 100;
+while (countdown > 0) {
+  countdown--;
+  if(countdown == 0)
+  {
+   console.log(" disarm the bomb");
+  }
+}
